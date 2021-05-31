ec93fe5525543e5074e5657a124d6dc9f43e12bd
ngocdh <huyngocbk@gmail.com>
2021-05-31 02:09:09 +0200

    practice mode 2

--- src/qmlmixerdlg.cpp
+++ src/qmlmixerdlg.cpp
@@ -7,10 +7,6 @@ QmlMixerDlg::QmlMixerDlg( CClient* pClnt )
 }
 
 void QmlMixerDlg::PracticeMode(){
-
-    if ( pClient->IsConnected() )
-        pClient->Stop();
     bPracticeMode = ! bPracticeMode;
     pClient->SetMuteOutStream( bPracticeMode );
-    pClient->Start();
 }
